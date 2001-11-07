1f12c33992a962ec0854234b481862b31b262732
pldms <shellac@users.noreply.github.com>
2001-11-07 20:03:53 +0000

    More ommissions. When will I learn?

---
+++ rdfweb.org/RDFAuthor/QueryResultSource.java
@@ -0,0 +1,40 @@
+//
+//  QueryResultSource.java
+//  RDFAuthor
+//
+//  Created by pldms on Tue Nov 06 2001.
+//  Copyright (c) 2001 __MyCompanyName__. All rights reserved.
+//
+
+import com.apple.cocoa.foundation.*;
+import com.apple.cocoa.application.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class QueryResultSource {
+    
+    ArrayList resultsArray;
+    
+    public QueryResultSource(ArrayList results)
+    {
+        resultsArray = results;
+    }
+    
+    public int numberOfRowsInTableView(NSTableView aTableView)
+    {
+        return resultsArray.size();
+    }
+    
+    public Object tableViewObjectValueForLocation( NSTableView aTableView, 
+                    NSTableColumn aTableColumn, int rowIndex)
+    {
+        String key = (String) aTableColumn.identifier();
+        return ((HashMap) resultsArray.get( rowIndex )).get(key);
+    }
+    
+    public HashMap getRow(int row)
+    {
+        return (HashMap) resultsArray.get( row );
+    }
+}
