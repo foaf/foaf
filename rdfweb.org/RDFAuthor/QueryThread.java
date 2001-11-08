d6abed83146b6da58044ab58161630644237aca3
pldms <shellac@users.noreply.github.com>
2001-11-08 16:13:56 +0000

    *** empty log message ***

---
+++ rdfweb.org/RDFAuthor/QueryThread.java
@@ -0,0 +1,65 @@
+//
+//  QueryThread.java
+//  RDFAuthor
+//
+//  Created by pldms on Thu Nov 08 2001.
+//  Copyright (c) 2001 __MyCompanyName__. All rights reserved.
+//
+
+// This does the actual query. It's a thread because these things can be - er - slow.
+// Axis doesn't do async yet
+
+import org.apache.axis.client.ServiceClient;
+import java.util.ArrayList;
+import java.lang.Thread;
+
+public class QueryThread extends Thread {
+    
+    QueryController owner;
+    ArrayList returned = null;
+    String endpoint="http://swordfish.rdfweb.org:8080/axis/servlet/AxisServlet";
+    String query = null;
+    String database = null;
+    long duration;
+    
+    public QueryThread(String query, String database, QueryController owner)
+    {
+        this.query = query;
+        this.database = database;
+        this.owner = owner;
+    }
+    
+    public void run()
+    {
+        try
+        {
+            // I'll time this (which is probably useful)
+            long startTime = java.util.Calendar.getInstance().getTime().getTime();
+            
+            ServiceClient client = new ServiceClient(endpoint);
+            
+            returned = (ArrayList) client.invoke(
+                                            "http://rdfweb.org/RDF/RDFWeb/SOAPDemo",
+                                            "squish",
+                                            new Object [] { query,database,"" });
+            duration = java.util.Calendar.getInstance().getTime().getTime() - startTime;
+        }
+        catch (Exception e)
+        {
+            owner.queryDied(e);
+        }
+        
+        owner.queryCompleted();
+    }
+
+    public ArrayList result()
+    {
+        return returned;
+    }
+    
+    public double duration()
+    {
+        return (double) duration/1000d;
+    }
+    
+}
