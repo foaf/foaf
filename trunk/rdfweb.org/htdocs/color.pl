1fcda89db215578f9ffe7906c210d7856443ce1f
danbri <danbri@9206d5c0-b91c-0410-8f36-fcd200ff9157>
2002-01-26 18:06:03 +0000

    improved layout. split into two colour choice - navbar and page highlights

--- trunk/rdfweb.org/htdocs/color.pl
+++ trunk/rdfweb.org/htdocs/color.pl
@@ -1,7 +1,12 @@
 #!/usr/bin/perl 
 #
+# generate index.html from index.html.orig 
+# using these two colours (in-page highlights and top-page navbar)
+# ./color.pl ffcc66 cccccc
+#
 # http://www.visibone.com/colorlab/big.html
 # http://rogers.fiendsreunited.com/people/danbri/remote-cvs/fireball.danbri.org/rdfweb.org/htdocs/
+# http://www.stone.com/java/cc/ColorCoordinator.html
 
 my $orig = 'index.html.orig';
 my $page = `cat $orig`;
@@ -12,9 +17,9 @@ my $c=shift || die 'no new colour';
 
 $page =~ s/#fffacd/#$c/gi;
 
-my $b = shift || '000000';
+my $b = shift || 'cccccc';
 
-$page =~ s/#000000/#$b/gi;
+$page =~ s/#cccccc/#$b/gi;
 
 open(OUT,">index.html") || die "can't write file $_";
 print OUT $page;
