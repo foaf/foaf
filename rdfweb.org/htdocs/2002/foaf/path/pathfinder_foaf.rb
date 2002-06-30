80f1d3b18913287bab47b2f0405a39a44fe4c5b9
Dan Brickley <danbri@users.noreply.github.com>
2002-06-30 21:44:59 +0000

    OK, got basic foafpath stuff translated from Damian and Libby's original Java. Not optimised, and haven't hooked it up to RDBMS or other store. foafpath.rb is the main thing, pathfinder_foaf.rb is just a way of getting the raw info from an RDFWeb aggregator.

---
+++ rdfweb.org/htdocs/2002/foaf/path/pathfinder_foaf.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby
+#
+# RDFWeb CoDepiction index generator
+#
+# Look up photos do-depicting some person, via their mailbox.
+# Generate HTML.
+# Assumes data has been drafragmented / smushed.
+#
+# danbri $Id: pathfinder_foaf.rb,v 1.1 2002-06-30 21:44:59 danbri Exp $
+
+require 'squish'
+require 'dbi'
+
+# see http://rdfweb.org/people/damian/2002/02/foafnation/
+
+
+# todo: fix squish parser so final comma not needed here:
+query=<<EOQ;
+
+	SELECT ?mbox1, ?mbox2, ?uri,  
+	WHERE
+       (foaf::depiction ?x ?img)
+       (foaf::depiction ?z ?img)
+       (foaf::thumbnail ?img ?uri)
+       (foaf::mbox ?x ?mbox1)
+       (foaf::mbox ?z ?mbox2)
+	USING foaf for http://xmlns.com/foaf/0.1/
+
+EOQ
+
+DBI_DRIVER = 'DBI:Pg:test1'
+DBI_USER = 'danbri'
+DBI_PASS=''
+
+q = SquishQuery.new.parseFromText query
+
+DBI.connect( DBI_DRIVER, DBI_USER, DBI_PASS) do | dbh |
+  dbh.select_all( q.toSQLQuery  ) do | row |
+    puts row.inspect
+ 
+   end
+end
+
