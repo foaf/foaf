3f55397dc331c4556b267f357dc173662de021f7
Dan Brickley <danbri@users.noreply.github.com>
2002-07-14 13:56:36 +0000

    progress on windows front (and mysql etc...).

--- rdfweb.org/htdocs/2002/foaf/scutter/conf/scutterdb.sql
+++ rdfweb.org/htdocs/2002/foaf/scutter/conf/scutterdb.sql
@@ -1,6 +1,6 @@
 -- create basic tables needed by rubyrdf
 -- usage: psql rdfweb1 < conf/scutterdb.sql
-
+-- do 'drop table ...' to destroy stored data.
 
 CREATE TABLE "triples" (
 	"subject" integer,
@@ -11,62 +11,14 @@ CREATE TABLE "triples" (
 	"isresource" boolean
 );
 
---
--- TOC Entry ID 3 (OID 31578)
---
--- Name: resources Type: TABLE Owner: pgsql
---
-
 CREATE TABLE "resources" (
 	"keyhash" integer,
-	"value" character varying
-);
-
---
--- TOC Entry ID 4 (OID 16569)
---
--- Name: "sub_index" Type: INDEX Owner: pgsql
---
+	"value" character varying );
 
 CREATE INDEX sub_index ON triples USING btree (subject);
-
---
--- TOC Entry ID 5 (OID 16570)
---
--- Name: "pred_index" Type: INDEX Owner: pgsql
---
-
 CREATE INDEX pred_index ON triples USING btree (predicate);
-
---
--- TOC Entry ID 6 (OID 16571)
---
--- Name: "obj_index" Type: INDEX Owner: pgsql
---
-
 CREATE INDEX obj_index ON triples USING btree (object);
-
---
--- TOC Entry ID 7 (OID 31583)
---
--- Name: "resources_key_key" Type: INDEX Owner: pgsql
---
-
-CREATE UNIQUE INDEX resources_key_key ON resources USING btree ("key");
-
---
--- TOC Entry ID 8 (OID 31584)
---
--- Name: "res_key_index" Type: INDEX Owner: pgsql
---
-
-CREATE INDEX res_key_index ON resources USING btree ("key");
-
---
--- TOC Entry ID 9 (OID 31585)
---
--- Name: "res_val_index" Type: INDEX Owner: pgsql
---
-
+CREATE UNIQUE INDEX resources_key_key ON resources USING btree ("keyhash");
+CREATE INDEX res_key_index ON resources USING btree ("keyhash");
 CREATE INDEX res_val_index ON resources USING btree (value);
 
