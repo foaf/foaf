5440dde877f11a2d73e547ddbdcf2548dc263fb3
danbri <danbri@9206d5c0-b91c-0410-8f36-fcd200ff9157>
2002-08-18 18:58:12 +0000

    irc logging script

---
+++ trunk/rdfweb.org/htdocs/irc/flogger.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+cd  /usr/local/rdfweb/from-cvs/htdocs/; \
+/usr/local/bin/irclogger -html -nick flogger ffff \
+        irc://irc.openprojects.net/foaf RDFWeb-FOAF foaf/ \
+        http://rdfweb.org/irc/foaf/irc.openprojects.net:6667/foaf/
+
