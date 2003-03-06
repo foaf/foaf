6fd7882f39fbab9e146ab26894cfc0083fd8a5ed
pldms <pldms@9206d5c0-b91c-0410-8f36-fcd200ff9157>
2003-03-06 18:33:46 +0000

    Die if addr loopback

--- trunk/rdfweb.org/software/foaffinger/src/org/rdfweb/foafcon/Util.java
+++ trunk/rdfweb.org/software/foaffinger/src/org/rdfweb/foafcon/Util.java
@@ -97,25 +97,13 @@ public class Util
       {
 	System.out.println(allHostInfo[i]);
 	
-	if (!allHostInfo[i].getAddress().equals("127.0.0.1"))
+	if (!allHostInfo[i].getHostAddress().equals("127.0.0.1"))
 	  return allHostInfo[i];
 	
 	
-        if(allHostInfo[i].isLoopbackAddress())
-	  System.out.println("loop back address");
-	else if(allHostInfo[i].isLinkLocalAddress())
-	  {
-	    System.out.println("link local address");
-	  }
-	
-	  
-        else if(allHostInfo[i].isSiteLocalAddress())
-	  System.out.println("site local address");
-	else if(allHostInfo[i].isAnyLocalAddress())
-	  System.out.println("local addresses");
       }
 
-    return InetAddress.getLocalHost();
+    throw new Exception("Can't get a non-loopback address");
     
   }
   
