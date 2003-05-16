102a55111f2400f7d6bb6f9c2ad3a718a44e9e2b
pldms <pldms@9206d5c0-b91c-0410-8f36-fcd200ff9157>
2003-05-16 10:39:49 +0000

    Beginning better structure for storing people.

---
+++ trunk/rdfweb.org/software/foaffinger/src/org/rdfweb/foafcon/People.java
@@ -0,0 +1,15 @@
+package org.rdfweb.foafcon;
+
+import java.util.*;
+
+public class People
+{
+  ArrayList people;
+
+  RendTest controller;
+
+  public People(RendTest controller)
+  {
+    this.controller = controller;
+
+    people = 
