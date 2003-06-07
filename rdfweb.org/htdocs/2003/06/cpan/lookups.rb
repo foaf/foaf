0b14665bfebee480609d80c56ef5feb5872384a0
Dan Brickley <danbri@users.noreply.github.com>
2003-06-07 22:04:55 +0000

    regenerated, fixed bugs, etc.

--- rdfweb.org/htdocs/2003/06/cpan/lookups.rb
+++ rdfweb.org/htdocs/2003/06/cpan/lookups.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/ruby
 #
-# $Id: lookups.rb,v 1.1 2003-06-07 17:44:55 danbri Exp $
+# $Id: lookups.rb,v 1.2 2003-06-07 22:04:55 danbri Exp $
 require 'squish'
 require 'basicrdf'
 require 'dbi'
@@ -11,6 +11,16 @@ oldq = SquishQuery.new.parseFromText "SELECT ?name, ?homepage, ?mb, ?x,
 		(foaf::homepage ?x ?homepage)
 	USING 	foaf for http://xmlns.com/foaf/0.1/"
 
+q = SquishQuery.new.parseFromText "SELECT ?name, ?homepage, ?weblog, ?x, 
+	WHERE 	(foaf::name ?x ?name)
+		(dc::contributor ?x http://www.cpan.org/)
+		(foaf::homepage ?x ?homepage)
+		(foaf::weblog ?x ?weblog)
+	USING 	dc for http://purl.org/dc/elements/1.1/
+		foaf for http://xmlns.com/foaf/0.1/"
+
+##		(dc::contributor ?x http://www.cpan.org/)
+
 
 
 q = SquishQuery.new.parseFromText "SELECT ?name, ?homepage, ?weblog, ?x, 
@@ -20,14 +30,6 @@ q = SquishQuery.new.parseFromText "SELECT ?name, ?homepage, ?weblog, ?x,
 		(foaf::weblog ?x ?weblog)
 	USING 	dc for http://purl.org/dc/elements/1.1/
 		foaf for http://xmlns.com/foaf/0.1/"
-
-q = SquishQuery.new.parseFromText "SELECT ?name, ?weblog, ?x, 
-	WHERE 	(foaf::name ?x ?name)
-		(dc::contributor ?x http://www.cpan.org/)
-		(foaf::weblog ?x ?weblog)
-	USING 	dc for http://purl.org/dc/elements/1.1/
-		foaf for http://xmlns.com/foaf/0.1/"
-
 
 dbi_driver = 'DBI:Pg:scutter1'
 DBI.connect(  dbi_driver, 'danbri' ,  '' ) do | dbh |
