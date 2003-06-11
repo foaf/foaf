a71947643c9bbcf40db8437e100ee1ed3d9e9271
danbri <danbri@9206d5c0-b91c-0410-8f36-fcd200ff9157>
2003-06-11 09:34:33 +0000

    regen'd with rssfeed

--- trunk/rdfweb.org/htdocs/2003/06/cpan/lookups.rb
+++ trunk/rdfweb.org/htdocs/2003/06/cpan/lookups.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/ruby
 #
-# $Id: lookups.rb,v 1.3 2003-06-11 09:30:04 danbri Exp $
+# $Id: lookups.rb,v 1.4 2003-06-11 09:34:33 danbri Exp $
 #
 # Lookup data from local SQL-backed RDF store.
 
@@ -8,18 +8,19 @@ require 'squish'
 require 'basicrdf'
 require 'dbi'
 
-q = SquishQuery.new.parseFromText "SELECT ?name, ?homepage, ?weblog, ?x, 
+q = SquishQuery.new.parseFromText "SELECT ?name, ?homepage, ?weblog, ?x, ?feed, 
 	WHERE 	(foaf::name ?x ?name)
 		(dc::contributor ?x http://www.cpan.org/)
 		(foaf::homepage ?x ?homepage)
 		(foaf::weblog ?x ?weblog)
+		(foaf::rssfeed ?weblog ?feed)
 	USING 	dc for http://purl.org/dc/elements/1.1/
 		foaf for http://xmlns.com/foaf/0.1/"
 
-DBI.connect(  'DBI:Pg:scutter1', 'danbri' ,  '' ) do | dbh |
-  dbh.select_all( q.toSQLQuery  ) do | x |
-    p=ResultRow.new x
-    puts "#{p.name} (#{p.homepage} weblog: #{p.weblog})\n";
+DBI.connect('DBI:Pg:scutter1', 'danbri' ,  '') do |dbh|
+  dbh.select_all(q.toSQLQuery) do |res|
+    p=ResultRow.new res
+    puts "#{p.name} homepage: #{p.homepage} weblog: #{p.weblog} rss: #{p.feed}\n";
   end
 end 
 
