6103c86ba590e1680c52d447dcb0439633d91c08
pldms <pldms@9206d5c0-b91c-0410-8f36-fcd200ff9157>
2004-08-15 20:38:05 +0000

    SWT continues...

--- trunk/rdfweb.org/software/foaffinger/src/org/rdfweb/foafcon/GUI.java
+++ trunk/rdfweb.org/software/foaffinger/src/org/rdfweb/foafcon/GUI.java
@@ -9,13 +9,18 @@ package org.rdfweb.foafcon;
 import org.eclipse.jface.window.ApplicationWindow;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.SWTException;
+import org.eclipse.swt.custom.SashForm;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.TabFolder;
+import org.eclipse.swt.widgets.TabItem;
 import org.eclipse.swt.widgets.Table;
-import org.eclipse.swt.widgets.TableColumn;
 import org.eclipse.swt.widgets.TableItem;
+import org.eclipse.swt.widgets.Text;
 
 /**
  * @author pldms
@@ -28,7 +33,12 @@ public class GUI extends ApplicationWindow implements UI
     private Table table;
     private Image presentImage;
     private Image absentImage;
-
+    private Text nameText;
+    private Text mboxText;
+    private Text homepageText;
+    private Text seeAlsoText;
+    private Text interestText;
+    
     public GUI()
 	{
 		super(null);
@@ -43,7 +53,21 @@ public class GUI extends ApplicationWindow implements UI
 	
 	protected Control createContents(Composite parent)
 	{
-		table = new Table(parent, SWT.CHECK);
+	    getShell().setText("Foaf Finger");
+	    
+	    /*TabFolder tabFolder = new TabFolder(parent, SWT.NONE);
+	    
+	    TabItem tabItem = new TabItem(tabFolder, SWT.NONE);
+	    
+	    tabItem.setText("My Details");
+	    
+	    tabItem = new TabItem(tabFolder, SWT.NONE);
+	    
+	    tabItem.setText("Local Network");
+	    */
+	    SashForm sash = new SashForm(parent, SWT.HORIZONTAL);
+	    
+		table = new Table(sash, SWT.CHECK);
 		
 		try
 		{
@@ -64,7 +88,38 @@ public class GUI extends ApplicationWindow implements UI
 		abs.setImage(absentImage);
 		abs.setText("Libby");*/
 		
-		return table;
+		Composite container = new Composite(sash, SWT.NONE);
+		GridLayout layout = new GridLayout();
+		layout.numColumns = 2;
+		
+		container.setLayout(layout);
+		
+		Label label = new Label(container, SWT.NONE);
+		label.setText("Name:");
+		nameText = new Text(container, SWT.SINGLE | SWT.READ_ONLY);
+		nameText.setText("Name");
+		
+		label = new Label(container, SWT.NONE);
+		label.setText("Mail Hash:");
+		mboxText = new Text(container, SWT.SINGLE | SWT.READ_ONLY);
+		mboxText.setText("0x39DF908G");
+		
+		label = new Label(container, SWT.NONE);
+		label.setText("Homepage:");
+		homepageText = new Text(container, SWT.SINGLE | SWT.READ_ONLY);
+		homepageText.setText("http://example.com/");
+		
+		label = new Label(container, SWT.NONE);
+		label.setText("Interest:");
+		interestText = new Text(container, SWT.SINGLE | SWT.READ_ONLY);
+		interestText.setText("http://example.com/foaffinger/");
+		
+		label = new Label(container, SWT.NONE);
+		label.setText("See Also:");
+		seeAlsoText = new Text(container, SWT.SINGLE | SWT.READ_ONLY);
+		seeAlsoText.setText("http://example.com/me.rdf");
+		
+		return sash;
 	}
 
 	/* (non-Javadoc)
@@ -85,6 +140,14 @@ public class GUI extends ApplicationWindow implements UI
 	        		
 	        		item.setImage(presentImage);
 	        		item.setText(message.getPerson().getName());
+	        		
+	        		Person person = message.getPerson();
+	        		
+	        		setField(nameText, person.getName());
+	        		setField(mboxText, person.getMboxHash());
+	        		setField(homepageText, person.getHomepage());
+	        		setField(interestText, person.getInterest());
+	        		setField(seeAlsoText, person.getSeeAlso());
 	        }
 	    else if (message instanceof Message.PersonOffline)
 	        {
@@ -93,7 +156,15 @@ public class GUI extends ApplicationWindow implements UI
 	        }
 	    else
 	        System.out.println("Message: " + message.getMessage());
-	    }});
+	    }
+
+        private void setField(Text nameText, String name)
+        {
+            if (name == null)
+                nameText.setText("");
+            else
+                nameText.setText(name);
+        }});
 	}
 	
 	
